c00223646e7402cb8fdfca386ba1c8971c402c44
completelyAbsorbed <completelyAbsorbed@users.noreply.github.com>
2023-05-07 20:21:14 -0600

    Update scrape_rates.R
    annotated process of pulling and cleaning release date, next -> functionalize this process

--- in-development/scrape_rates.R
+++ in-development/scrape_rates.R
@@ -2,13 +2,31 @@
 
 library(tidyverse)
 library(rvest)
+library(lubridate)
 
+# define the url we will pull from
 webpage_h15 <- "https://www.federalreserve.gov/releases/h15/"
 
+# define the selector for the release date
 selector_date <- ".dates"
 
+# read the webpage
 html_h15 <- read_html(webpage_h15)
 
-date_release <- html_h15 |> html_elements(selector_date)
+# pull the release date element
+raw_date_release <- html_h15 |> html_elements(selector_date)
 
+# define a character variable from the xml_nodeset element for release date
+date_release <- html_text2(raw_date_release)
+
+# define the chaff to separate from date_release
+chaff_release_date <- "Release date: "
+
+# trim the tags and unnecessary text from date_release
+date_release_trimmed <- str_remove(string = date_release,
+                                   pattern = chaff_release_date)
+
+
+# finalize processing of the element into Date() format
+date_of_release <- lubridate::mdy(date_release_trimmed)
 
