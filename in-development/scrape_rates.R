37c93874937f00b48a131856c356012ad13ae08f
completelyAbsorbed <completelyAbsorbed@users.noreply.github.com>
2023-05-07 20:08:07 -0600

    Update scrape_rates.R
    pulling the right piece from the fed rates date, need to trim it to size

--- in-development/scrape_rates.R
+++ in-development/scrape_rates.R
@@ -5,8 +5,10 @@ library(rvest)
 
 webpage_h15 <- "https://www.federalreserve.gov/releases/h15/"
 
-selector_date <- ".date" # //*[contains(concat( " ", @class, " " ), concat( " ", "dates", " " ))]
+selector_date <- ".dates"
 
 html_h15 <- read_html(webpage_h15)
 
-html_h15 |> html_elements(selector_date)
+date_release <- html_h15 |> html_elements(selector_date)
+
+
